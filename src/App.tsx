2249f9c437487da5fed18d17b8a892cc4c73b3e3
Yousseftame <tamerosama73@gmail.com>
2025-07-25 23:49:01 +0300

    reusable layout-form-input-button

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,46 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import ForgetPassword from "./forget-password/ForgetPassword"
+import { Toaster } from 'react-hot-toast'
+import ResetPassword from './reset-password/ResetPassword'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+import AuthLayout from './authLayOut/AuthLayout'
+import Login from './login/Login'
 function App() {
-  const [count, setCount] = useState(0)
+   const Routes = createHashRouter([
+    {
+    
+  
+      path:'',
+      element:<AuthLayout/>,
+      children:[
+        {
+          path:'', 
+          element:<Login/>
+          
+        },
+         {
+          path:'forget-password', 
+          element:  <ForgetPassword/>
+
+          
+        },
+         {
+          path:'reset-password', 
+          element: <ResetPassword/>
+
+          
+        }
+      ],
+      
+    }
+    ])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  <RouterProvider router={Routes}></RouterProvider>
+  <Toaster />
     </>
   )
 }
