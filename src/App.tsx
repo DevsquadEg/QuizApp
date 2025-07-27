c8c6fdadd132fa3dad5d352ae33ea9dfd5451993
onisEg <anasabdo704@gmail.com>
2025-07-27 13:47:48 +0200

    #

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,25 @@ import "./App.css";
 import ForgetPassword from "./forget-password/ForgetPassword";
 import { Toaster } from "react-hot-toast";
 import { createHashRouter, RouterProvider } from "react-router-dom";
-import AuthLayout from "./layouts/authLayOut/AuthLayout";
 import Login from "./login/Login";
 import Register from "./pages/authPages/register/Register";
 import NotFound from "./not-found/NotFound";
 import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
 import Dashboard from "./dashboard/Dashboard";
 import ResetPassword from "./pages/authPages/reset-password/ResetPassword";
+import AuthLayout from "./layouts/authLayOut/AuthLayout";
 function App() {
   const Routes = createHashRouter([
     {
-      path: "",
+      path: "/",
       element: <AuthLayout />,
       errorElement: <NotFound />,
       children: [
-        {
-          index: true,
-          element: <Login />,
-        },
-        {
-          path: "login",
-          element: <Login />,
-        },
-        {
-          path: "register",
-          element: <Register />,
-        },
-        {
-          path: "forget-password",
-          element: <ForgetPassword />,
-        },
-        {
-          path: "reset-password",
-          element: <ResetPassword />,
-        },
+        { index: true, element: <Login /> }, // "/"
+        { path: "login", element: <Login /> },
+        { path: "register", element: <Register /> },
+        { path: "forget-password", element: <ForgetPassword /> },
+        { path: "reset-password", element: <ResetPassword /> },
       ],
     },
     {
@@ -49,11 +34,15 @@ function App() {
         },
       ],
     },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
   ]);
 
   return (
     <>
-      <RouterProvider router={Routes}></RouterProvider>
+      <RouterProvider router={Routes} />
       <Toaster />
     </>
   );
